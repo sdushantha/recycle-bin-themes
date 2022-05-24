dbf3f5b4a12903f0c38be84ef7435869752cde43
TechnoLuc <94373293+technoluc@users.noreply.github.com>
2022-05-24 20:06:09 +0200

    add dachshund

--- binutil.ps1
+++ binutil.ps1
@@ -52,14 +52,20 @@ $minecraftempty = "https://raw.githubusercontent.com/technoluc/recycle-bin-theme
 #minecraft-full.ico
 $minecraftfull = "https://raw.githubusercontent.com/technoluc/recycle-bin-themes/main/themes/minecraft-chest/minecraft-chest-full.ico"
 
+#dachshund-empty.ico
+$dachshundempty = "https://raw.githubusercontent.com/technoluc/recycle-bin-themes/main/themes/dachshund/dachshund-empty.ico"
+
+#dachshund-full.ico
+$dachshundfull = "https://raw.githubusercontent.com/technoluc/recycle-bin-themes/main/themes/dachshund/dachshund-full.ico"
+
 #Get user choice
 Write-Host "Please choose your theme:"
-$choice = $(Write-Host "[1]Pop-Cat `n[2]Patrik Star `n[3]Kirby `n[4]Sword Kirby `n[5]Bulbasaul `n[6]Minecraft Chest `n[9]Default " -ForegroundColor Yellow) + $(Write-Host "Choice: " -NoNewLine; Read-Host)
-if ($choice -eq '1' -or $choice -eq '2' -or $choice -eq '3' -or $choice -eq '4' -or $choice -eq '5' -or $choice -eq '6' -or $choice -eq '9') {
+$choice = $(Write-Host "[1]Pop-Cat `n[2]Patrik Star `n[3]Kirby `n[4]Sword Kirby `n[5]Bulbasaul `n[6]Minecraft Chest `n[7]Dachshund `n[9]Default " -ForegroundColor Yellow) + $(Write-Host "Choice: " -NoNewLine; Read-Host)
+if ($choice -eq '1' -or $choice -eq '2' -or $choice -eq '3' -or $choice -eq '4' -or $choice -eq '5' -or $choice -eq '6' -or $choice -eq '7' -or $choice -eq '9') {
 Write-Host "You picked" $choice
 }else{
 Write-Host "You picked" $choice 
-Write-Host "Please enter 1, 2, 3, 4, 5,6 or 9."
+Write-Host "Please enter 1, 2, 3, 4, 5, 6, 7 or 9."
 }
 
 if ($choice -eq '1')
@@ -158,6 +164,22 @@ Set-ItemProperty -Path "Registry::HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\C
 Set-ItemProperty -Path "Registry::HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{645FF040-5081-101B-9F08-00AA002F954E}\DefaultIcon" -Name "empty" -Value "$env:userprofile\icons\minecraft-chest-empty.ico,0"
 }
 
+if ($choice -eq '6')
+{
+Write-Host "Downloading Dachshund Empty Icon"
+$url = $dachshundempty
+$output = "$env:userprofile\icons\dachshund-empty.ico"
+Invoke-WebRequest -Uri $url -OutFile ( New-Item -Path "$output" -Force )
+Write-Host "Downloading Dachshund Full Icon"
+$url = $dachshundfull
+$output = "$env:userprofile\icons\dachshund-full.ico"
+Write-Host "Setting Registry"
+Invoke-WebRequest -Uri $url -OutFile ( New-Item -Path "$output" -Force )
+Set-ItemProperty -Path "Registry::HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{645FF040-5081-101B-9F08-00AA002F954E}\DefaultIcon" -Name "(Default)" -Value "$env:userprofile\icons\dachshund-empty.ico,0"
+Set-ItemProperty -Path "Registry::HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{645FF040-5081-101B-9F08-00AA002F954E}\DefaultIcon" -Name "full" -Value "$env:userprofile\icons\dachshund-full.ico,0"
+Set-ItemProperty -Path "Registry::HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{645FF040-5081-101B-9F08-00AA002F954E}\DefaultIcon" -Name "empty" -Value "$env:userprofile\icons\
+dachshund-empty.ico,0"
+}
 
 if ($choice -eq '9')
 {
