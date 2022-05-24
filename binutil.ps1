2df5f9da6eaa1eb01251f8a5c3a1c0840423210a
technoluc <luc@technoluc.nl>
2022-05-24 15:55:38 +0200

    add minecraft chest

--- binutil.ps1
+++ binutil.ps1
@@ -46,17 +46,20 @@ $bulbasaulempty = "https://raw.githubusercontent.com/technoluc/recycle-bin-theme
 #bulbasaul-full.ico
 $bulbasaulfull = "https://raw.githubusercontent.com/technoluc/recycle-bin-themes/main/themes/bulbasaul/bulbasaul-full.ico"
 
+#minecraft-empty.ico
+$minecraftempty = "https://raw.githubusercontent.com/technoluc/recycle-bin-themes/main/themes/minecraft-chest/minecraft-chest-empty.ico"
 
+#minecraft-full.ico
+$minecraftfull = "https://raw.githubusercontent.com/technoluc/recycle-bin-themes/main/themes/minecraft-chest/minecraft-chest-full.ico"
 
 #Get user choice
 Write-Host "Please choose your theme:"
-$choice = $(Write-Host "[1]Pop-Cat `n[2]Patrik Star `n[3]Kirby `n[4]Sword Kirby `n[5]Bulbasaul `n[6]Default " -ForegroundColor Yellow) + $(Write-Host "Choice: " -NoNewLine; Read-Host)
-# $choice = Read-Host "Enter [1]Pop-Cat, [2]Patrik Star, [3]Kirby, [4]Sword Kirby, [5]Bulbasaul or [6]Default"
-if ($choice -eq '1' -or $choice -eq '2' -or $choice -eq '3' -or $choice -eq '4' -or $choice -eq '5' -or $choice -eq '6') {
+$choice = $(Write-Host "[1]Pop-Cat `n[2]Patrik Star `n[3]Kirby `n[4]Sword Kirby `n[5]Bulbasaul `n[6]Minecraft Chest `n[9]Default " -ForegroundColor Yellow) + $(Write-Host "Choice: " -NoNewLine; Read-Host)
+if ($choice -eq '1' -or $choice -eq '2' -or $choice -eq '3' -or $choice -eq '4' -or $choice -eq '5' -or $choice -eq '6' -or $choice -eq '9') {
 Write-Host "You picked" $choice
 }else{
 Write-Host "You picked" $choice 
-Write-Host "Please enter 1, 2, 3, 4, 5 or 6."
+Write-Host "Please enter 1, 2, 3, 4, 5,6 or 9."
 }
 
 if ($choice -eq '1')
@@ -140,6 +143,23 @@ Set-ItemProperty -Path "Registry::HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\C
 }
 
 if ($choice -eq '6')
+{
+Write-Host "Downloading Minecraft Chest Empty Icon"
+$url = $minecraftempty
+$output = "$env:userprofile\icons\minecraft-chest-empty.ico"
+Invoke-WebRequest -Uri $url -OutFile ( New-Item -Path "$output" -Force )
+Write-Host "Downloading Minecraft Chest Full Icon"
+$url = $minecraftfull
+$output = "$env:userprofile\icons\minecraft-chest-full.ico"
+Write-Host "Setting Registry"
+Invoke-WebRequest -Uri $url -OutFile ( New-Item -Path "$output" -Force )
+Set-ItemProperty -Path "Registry::HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{645FF040-5081-101B-9F08-00AA002F954E}\DefaultIcon" -Name "(Default)" -Value "$env:userprofile\icons\minecraft-chest-empty.ico,0"
+Set-ItemProperty -Path "Registry::HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{645FF040-5081-101B-9F08-00AA002F954E}\DefaultIcon" -Name "full" -Value "$env:userprofile\icons\minecraft-chest-full.ico,0"
+Set-ItemProperty -Path "Registry::HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{645FF040-5081-101B-9F08-00AA002F954E}\DefaultIcon" -Name "empty" -Value "$env:userprofile\icons\minecraft-chest-empty.ico,0"
+}
+
+
+if ($choice -eq '9')
 {
 Write-Host "Resetting to default settings"
 Set-ItemProperty -Path "Registry::HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{645FF040-5081-101B-9F08-00AA002F954E}\DefaultIcon" -Name "(Default)" -Value "%SystemRoot%\System32\imageres.dll,-55"
