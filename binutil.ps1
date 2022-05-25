297d3d57f481d7db884b34df08d80c39ec20bef5
TechnoLuc <94373293+technoluc@users.noreply.github.com>
2022-05-25 11:59:06 +0200

    comment out elevation since it isn't necessary
    BUT IT SHOULD WORK ANYWAY

--- binutil.ps1
+++ binutil.ps1
@@ -4,13 +4,14 @@
 ##################################################################################################################
 
 # Self-elevate the script if required
-if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
+<# if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
  if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
   $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
   Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList $CommandLine
   Exit
  }
 }
+#>
 
 # Set the working location to the same location as the script
 # Write-Host "Setting Working Directory to Script Root"
