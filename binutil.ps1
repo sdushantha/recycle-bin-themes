9ef45da61017caa168fa6018537b742d9f6fcd26
technoluc <info@technoluc.nl>
2022-05-25 19:42:59 +0200

    format document

--- binutil.ps1
+++ binutil.ps1
@@ -5,11 +5,11 @@
 
 # Self-elevate the script if required
 <# if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
- if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
-  $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
-  Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList $CommandLine
-  Exit
- }
+  if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
+    $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
+    Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList $CommandLine
+    Exit
+  }
 }
 #>
 
@@ -80,31 +80,31 @@ $dachshundfulloutput = "$env:userprofile\icons\dachshund-full.ico"
 Write-Host "Please choose your theme:"
 $choice = $(Write-Host "[1]Pop-Cat `n[2]Patrik Star `n[3]Kirby `n[4]Sword Kirby `n[5]Bulbasaul `n[6]Minecraft Chest `n[7]Dachshund `n[9]Default " -ForegroundColor Yellow) + $(Write-Host "Choice: " -NoNewLine; Read-Host)
 if ($choice -eq '1' -or $choice -eq '2' -or $choice -eq '3' -or $choice -eq '4' -or $choice -eq '5' -or $choice -eq '6' -or $choice -eq '7' -or $choice -eq '9') {
-    Write-Host "You picked" $choice
+  Write-Host "You picked" $choice
 }
 else {
-    Write-Host "You picked" $choice 
-    Write-Host "Please enter 1, 2, 3, 4, 5, 6, 7 or 9."
+  Write-Host "You picked" $choice 
+  Write-Host "Please enter 1, 2, 3, 4, 5, 6, 7 or 9."
 }
 
 if ($choice -eq '1') {
 
   if (-not(Test-Path -Path $pcatemptyoutput -PathType Leaf)) {
-      Write-Host "Downloading pop-cat Empty Icon"
-      $url = $pcatempty
-      Invoke-WebRequest -Uri $url -OutFile ( New-Item -Path "$pcatemptyoutput" -Force )
+    Write-Host "Downloading pop-cat Empty Icon"
+    $url = $pcatempty
+    Invoke-WebRequest -Uri $url -OutFile ( New-Item -Path "$pcatemptyoutput" -Force )
   }
   else {
-      Write-Host "Pop-cat Empty Icon already exists"
+    Write-Host "Pop-cat Empty Icon already exists"
   }
 
   if (-not(Test-Path -Path $pcatfulloutput -PathType Leaf)) {
-      Write-Host "Downloading pop-cat Full Icon"
-      $url = $pcatfull
-      Invoke-WebRequest -Uri $url -OutFile ( New-Item -Path "$pcatfulloutput" -Force )
+    Write-Host "Downloading pop-cat Full Icon"
+    $url = $pcatfull
+    Invoke-WebRequest -Uri $url -OutFile ( New-Item -Path "$pcatfulloutput" -Force )
   }
   else {
-      Write-Host "Pop-cat Full Icon already exists"
+    Write-Host "Pop-cat Full Icon already exists"
   }
 
   Write-Host "Setting Registry"
@@ -116,21 +116,21 @@ if ($choice -eq '1') {
 if ($choice -eq '2') {
 
   if (-not(Test-Path -Path $pstaremptyoutput -PathType Leaf)) {
-      Write-Host "Downloading Patrick Star Empty Icon"
-      $url = $pstarempty
-      Invoke-WebRequest -Uri $url -OutFile ( New-Item -Path "$pstaremptyoutput" -Force )
+    Write-Host "Downloading Patrick Star Empty Icon"
+    $url = $pstarempty
+    Invoke-WebRequest -Uri $url -OutFile ( New-Item -Path "$pstaremptyoutput" -Force )
   }
   else {
-      Write-Host "Patrick Star Empty Icon already exists"
+    Write-Host "Patrick Star Empty Icon already exists"
   }
 
   if (-not(Test-Path -Path $pstarfulloutput -PathType Leaf)) {
-      Write-Host "Downloading Patrick Star Full Icon"
-      $url = $pstarfull
-      Invoke-WebRequest -Uri $url -OutFile ( New-Item -Path "$pstarfulloutput" -Force )
+    Write-Host "Downloading Patrick Star Full Icon"
+    $url = $pstarfull
+    Invoke-WebRequest -Uri $url -OutFile ( New-Item -Path "$pstarfulloutput" -Force )
   }
   else {
-      Write-Host "Patrick Star Full Icon already exists"
+    Write-Host "Patrick Star Full Icon already exists"
   }
 
   Write-Host "Setting Registry"
@@ -142,21 +142,21 @@ if ($choice -eq '2') {
 if ($choice -eq '3') {
 
   if (-not(Test-Path -Path $kirbyemptyoutput -PathType Leaf)) {
-      Write-Host "Downloading Kirby Empty Icon"
-      $url = $kirbyempty
-      Invoke-WebRequest -Uri $url -OutFile ( New-Item -Path "$kirbyemptyoutput" -Force )
+    Write-Host "Downloading Kirby Empty Icon"
+    $url = $kirbyempty
+    Invoke-WebRequest -Uri $url -OutFile ( New-Item -Path "$kirbyemptyoutput" -Force )
   }
   else {
-      Write-Host "Kirby Empty Icon already exists"
+    Write-Host "Kirby Empty Icon already exists"
   }
 
   if (-not(Test-Path -Path $kirbyfulloutput -PathType Leaf)) {
-      Write-Host "Downloading Kirby Full Icon"
-      $url = $kirbyfull
-      Invoke-WebRequest -Uri $url -OutFile ( New-Item -Path "$kirbyfulloutput" -Force )
+    Write-Host "Downloading Kirby Full Icon"
+    $url = $kirbyfull
+    Invoke-WebRequest -Uri $url -OutFile ( New-Item -Path "$kirbyfulloutput" -Force )
   }
   else {
-      Write-Host "Kirby Full Icon already exists"
+    Write-Host "Kirby Full Icon already exists"
   }
 
   Write-Host "Setting Registry"
@@ -168,21 +168,21 @@ if ($choice -eq '3') {
 if ($choice -eq '4') {
 
   if (-not(Test-Path -Path $skirbyemptyoutput -PathType Leaf)) {
-      Write-Host "Downloading Sword Kirby Empty Icon"
-      $url = $skirbyempty
-      Invoke-WebRequest -Uri $url -OutFile ( New-Item -Path "$skirbyemptyoutput" -Force )
+    Write-Host "Downloading Sword Kirby Empty Icon"
+    $url = $skirbyempty
+    Invoke-WebRequest -Uri $url -OutFile ( New-Item -Path "$skirbyemptyoutput" -Force )
   }
   else {
-      Write-Host "Sword Kirby Empty Icon already exists"
+    Write-Host "Sword Kirby Empty Icon already exists"
   }
 
   if (-not(Test-Path -Path $skirbyfulloutput -PathType Leaf)) {
-      Write-Host "Downloading Sword Kirby Full Icon"
-      $url = $skirbyfull
-      Invoke-WebRequest -Uri $url -OutFile ( New-Item -Path "$skirbyfulloutput" -Force )
+    Write-Host "Downloading Sword Kirby Full Icon"
+    $url = $skirbyfull
+    Invoke-WebRequest -Uri $url -OutFile ( New-Item -Path "$skirbyfulloutput" -Force )
   }
   else {
-      Write-Host "Sword Kirby Full Icon already exists"
+    Write-Host "Sword Kirby Full Icon already exists"
   }
 
   Write-Host "Setting Registry"
@@ -194,21 +194,21 @@ if ($choice -eq '4') {
 if ($choice -eq '5') {
 
   if (-not(Test-Path -Path $bulbasaulemptyoutput -PathType Leaf)) {
-      Write-Host "Downloading Bulbasaul Empty Icon"
-      $url = $bulbasaulempty
-      Invoke-WebRequest -Uri $url -OutFile ( New-Item -Path "$bulbasaulemptyoutput" -Force )
+    Write-Host "Downloading Bulbasaul Empty Icon"
+    $url = $bulbasaulempty
+    Invoke-WebRequest -Uri $url -OutFile ( New-Item -Path "$bulbasaulemptyoutput" -Force )
   }
   else {
-      Write-Host "Bulbasaul Empty Icon already exists"
+    Write-Host "Bulbasaul Empty Icon already exists"
   }
 
   if (-not(Test-Path -Path $bulbasaulfulloutput -PathType Leaf)) {
-      Write-Host "Downloading Bulbasaul Full Icon"
-      $url = $bulbasaulfull
-      Invoke-WebRequest -Uri $url -OutFile ( New-Item -Path "$bulbasaulfulloutput" -Force )
+    Write-Host "Downloading Bulbasaul Full Icon"
+    $url = $bulbasaulfull
+    Invoke-WebRequest -Uri $url -OutFile ( New-Item -Path "$bulbasaulfulloutput" -Force )
   }
   else {
-      Write-Host "Bulbasaul Full Icon already exists"
+    Write-Host "Bulbasaul Full Icon already exists"
   }
 
   Write-Host "Setting Registry"
@@ -220,21 +220,21 @@ if ($choice -eq '5') {
 if ($choice -eq '6') {
 
   if (-not(Test-Path -Path $minecraftemptyoutput -PathType Leaf)) {
-      Write-Host "Downloading Minecraft Chest Empty Icon"
-      $url = $minecraftempty
-      Invoke-WebRequest -Uri $url -OutFile ( New-Item -Path "$minecraftemptyoutput" -Force )
+    Write-Host "Downloading Minecraft Chest Empty Icon"
+    $url = $minecraftempty
+    Invoke-WebRequest -Uri $url -OutFile ( New-Item -Path "$minecraftemptyoutput" -Force )
   }
   else {
-      Write-Host "Minecraft Chest Empty Icon already exists"
+    Write-Host "Minecraft Chest Empty Icon already exists"
   }
 
   if (-not(Test-Path -Path $minecraftfulloutput -PathType Leaf)) {
-      Write-Host "Downloading Minecraft Chest Full Icon"
-      $url = $minecraftfull
-      Invoke-WebRequest -Uri $url -OutFile ( New-Item -Path "$minecraftfulloutput" -Force )
+    Write-Host "Downloading Minecraft Chest Full Icon"
+    $url = $minecraftfull
+    Invoke-WebRequest -Uri $url -OutFile ( New-Item -Path "$minecraftfulloutput" -Force )
   }
   else {
-      Write-Host "Minecraft Chest Full Icon already exists"
+    Write-Host "Minecraft Chest Full Icon already exists"
   }
 
   Write-Host "Setting Registry"
@@ -246,21 +246,21 @@ if ($choice -eq '6') {
 if ($choice -eq '7') {
 
   if (-not(Test-Path -Path $dachshundemptyoutput -PathType Leaf)) {
-      Write-Host "Downloading Dachshund Empty Icon"
-      $url = $dachshundempty
-      Invoke-WebRequest -Uri $url -OutFile ( New-Item -Path "$dachshundemptyoutput" -Force )
+    Write-Host "Downloading Dachshund Empty Icon"
+    $url = $dachshundempty
+    Invoke-WebRequest -Uri $url -OutFile ( New-Item -Path "$dachshundemptyoutput" -Force )
   }
   else {
-      Write-Host "Dachshund Empty Icon already exists"
+    Write-Host "Dachshund Empty Icon already exists"
   }
 
   if (-not(Test-Path -Path $dachshundfulloutput -PathType Leaf)) {
-      Write-Host "Downloading Dachshund Full Icon"
-      $url = $dachshundfull
-      Invoke-WebRequest -Uri $url -OutFile ( New-Item -Path "$dachshundfulloutput" -Force )
+    Write-Host "Downloading Dachshund Full Icon"
+    $url = $dachshundfull
+    Invoke-WebRequest -Uri $url -OutFile ( New-Item -Path "$dachshundfulloutput" -Force )
   }
   else {
-      Write-Host "Dachshund Full Icon already exists"
+    Write-Host "Dachshund Full Icon already exists"
   }
 
   Write-Host "Setting Registry"
@@ -269,16 +269,15 @@ if ($choice -eq '7') {
   Set-ItemProperty -Path "Registry::HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{645FF040-5081-101B-9F08-00AA002F954E}\DefaultIcon" -Name "empty" -Value "$dachshundemptyoutput,0"
 }
 
-if ($choice -eq '9')
-{
-Write-Host "Resetting to default settings"
-Set-ItemProperty -Path "Registry::HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{645FF040-5081-101B-9F08-00AA002F954E}\DefaultIcon" -Name "(Default)" -Value "%SystemRoot%\System32\imageres.dll,-55"
-Set-ItemProperty -Path "Registry::HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{645FF040-5081-101B-9F08-00AA002F954E}\DefaultIcon" -Name "full" -Value "%SystemRoot%\System32\imageres.dll,-54"
-Set-ItemProperty -Path "Registry::HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{645FF040-5081-101B-9F08-00AA002F954E}\DefaultIcon" -Name "empty" -Value "%SystemRoot%\System32\imageres.dll,-55"
+if ($choice -eq '9') {
+  Write-Host "Resetting to default settings"
+  Set-ItemProperty -Path "Registry::HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{645FF040-5081-101B-9F08-00AA002F954E}\DefaultIcon" -Name "(Default)" -Value "%SystemRoot%\System32\imageres.dll,-55"
+  Set-ItemProperty -Path "Registry::HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{645FF040-5081-101B-9F08-00AA002F954E}\DefaultIcon" -Name "full" -Value "%SystemRoot%\System32\imageres.dll,-54"
+  Set-ItemProperty -Path "Registry::HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{645FF040-5081-101B-9F08-00AA002F954E}\DefaultIcon" -Name "empty" -Value "%SystemRoot%\System32\imageres.dll,-55"
 }
 
 #Refresh Desktop
- $code = @'
+$code = @'
   [System.Runtime.InteropServices.DllImport("Shell32.dll")] 
   private static extern int SHChangeNotify(int eventId, int flags, IntPtr item1, IntPtr item2);
 
